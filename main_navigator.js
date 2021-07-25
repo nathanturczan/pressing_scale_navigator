c720cdd08246729efd772c8e8fd8627585c8bafb
rudytak <ondra4sedlacek@gmail.com>
2021-07-25 22:57:16 +0200

    Transfer to object oriented navigator

---
+++ main_navigator.js
@@ -0,0 +1,147 @@
+const default_animation_curve = (x) => { return (1 / (1 + pow(x / (1 - x), -3))) };
+
+class Navigator {
+    constructor() {
+        this.main_polygon
+        this.neighbors = [];
+        this.old_main_polygon
+        this.old_neighbors;
+        this.last_clicked_polygon, this.actually_new_polygons;
+        this.preview_polygons = []
+        this.poly_size = 125
+        this.preview_polygons_ready = false
+
+        // create the initial polygons
+        this.main_polygon = new Polygon(width / 2, height / 2, this.poly_size, "c_diatonic")
+        this.neighbors = this.main_polygon.getNeighbors();
+    }
+
+    draw() {
+        push()
+        ellipseMode(RADIUS);
+
+        //background(255);
+        var allPolygons = [this.main_polygon].concat(this.preview_polygons, this.old_neighbors)
+        allPolygons.push(...this.neighbors)
+        allPolygons.push(this.old_main_polygon)
+
+        //draw all the polygons
+        for (var p of allPolygons) {
+            if (p) p.draw();
+        }
+        pop()
+    }
+
+    mousePressed() {
+        // check for clicks on all polygons
+        for (var p of this.neighbors) {
+            if (p && p.click() && !p.animation.active) {
+                this.preview_polygons = p.getNeighbors()
+                this.last_clicked_polygon = p;
+
+                // duplicates between this.neighbors
+                var all_current = this.neighbors.concat([this.main_polygon])
+                this.actually_new_polygons = new Set();
+                for (var n = 0; n < all_current.length; n++) {
+                    for (var pre = 0; pre < this.preview_polygons.length; pre++) {
+                        if (all_current[n].isMatching(this.preview_polygons[pre])) {
+                            this.preview_polygons[pre] = all_current[n];
+                        }
+                    }
+                }
+
+                // find polygons which are actually new
+                for (var pre = 0; pre < this.preview_polygons.length; pre++) {
+                    if (!all_current.includes(this.preview_polygons[pre])) {
+                        this.actually_new_polygons.add(this.preview_polygons[pre])
+                    }
+                }
+
+                // convert to array
+                this.actually_new_polygons = Array.from(this.actually_new_polygons)
+
+                // take care of the fanning out (not all the way around)
+                var total_poly = this.neighbors.length;
+                var ind = this.main_polygon.getNeighbors().findIndex(x => x.isMatching(p))
+                    //var positions = p.getNeighborPositions(p.x, p.y, p.radius, undefined, undefined, PI / 2 + (2 * PI * (ind - 0.5)) / total_poly, PI / 2 + (2 * PI * (ind + 0.5)) / total_poly, this.actually_new_polygons.length)
+                var positions = p.getNeighborPositions(p.x, p.y, p.radius, undefined, undefined, PI / 2, PI / 2 + (2 * PI), this.actually_new_polygons.length)
+
+                /*
+                //position them
+                for (var a_n = 0; a_n < this.actually_new_polygons.length; a_n++) {
+                    var pol = this.preview_polygons.find(x => this.actually_new_polygons[a_n].isMatching(x))
+    
+                    pol.set(["x", positions[a_n].x], ["y", positions[a_n].y], ["size", positions[a_n].size])
+                }*/
+
+                for (var prev of this.actually_new_polygons) {
+                    var oldx = prev.x;
+                    var oldy = prev.y;
+
+                    prev.set(["x", p.x], ["y", p.y])
+                    prev.move(oldx, oldy, prev.size)
+                }
+
+                this.preview_polygons_ready = true;
+                return
+            }
+        }
+    }
+
+    mouseReleased() {
+        // check for clicks on all polygons
+        for (var p of this.neighbors) {
+            if (p && p.click() && !p.animation.active && this.preview_polygons_ready) {
+                this.changeMainScale(p)
+                return
+            }
+        }
+
+        if (this.actually_new_polygons) {
+            for (var prev of this.actually_new_polygons) {
+                if (!prev.animation.active) prev.move(this.last_clicked_polygon.x, this.last_clicked_polygon.y, 1, 0)
+            }
+        }
+
+        //this.preview_polygons = []
+        this.preview_polygons_ready = false;
+    }
+
+    changeMainScale(new_main, all_duration = 1) {
+        if (new_main == this.main_polygon) return
+
+        // push the current polygons into old polygons
+        this.old_neighbors = [...this.neighbors]
+        this.old_main_polygon = this.main_polygon;
+
+        this.main_polygon = new_main
+        this.neighbors = this.preview_polygons;
+
+        //Handle duplicates
+        this.old_neighbors.splice(this.old_neighbors.indexOf(this.main_polygon), 1)
+
+        // duplicate of main polygon
+        var index = this.neighbors.findIndex((x) => { return this.old_main_polygon.isMatching(x) });
+        if (index !== -1) {
+            this.neighbors[index] = this.old_main_polygon;
+        }
+
+        // Main polygons animation
+        this.main_polygon.move(width / 2, height / 2, all_duration, this.poly_size);
+        //this.old_main_polygon.move(width / 2, height / 2, all_duration, 0, 0)
+
+        var positions = this.main_polygon.getNeighborPositions(width / 2, height / 2, this.poly_size)
+        for (var i = 0; i < this.neighbors.length; i++) {
+            try {
+                this.neighbors[i].move(positions[i].x, positions[i].y, all_duration, positions[i].size, 1)
+            } catch (error) {}
+        }
+
+        // Neighboring polygons animation
+        for (var old of this.old_neighbors) {
+            if (this.neighbors.findIndex(x => old.isMatching(x)) == -1) {
+                old.move(this.old_main_polygon.animation.target.x, this.old_main_polygon.animation.target.y, all_duration, 0, 1)
+            }
+        }
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,142 +1,25 @@
-var renderer;
-var main_polygon, neighbors = [];
-var old_main_polygon, old_neighbors;
-var last_clicked_polygon, actually_new_polygons;
-var preview_polygons = []
-var global_size = 125
-var preview_polygons_ready = false
-
 const fps = 30;
 const note_names = ["C", "D♭", "D", "E♭", "E", "F", "F#", "G", "A♭", "A", "B♭", "B"];
-const default_animation_curve = (x) => { return (1 / (1 + pow(x / (1 - x), -3))) };
+
+var nav;
 
 function setup() {
-    ellipseMode(RADIUS);
     renderer = createCanvas(window.innerWidth, window.innerHeight);
     frameRate(fps); //there are other ways to do timing, like setInterval()
 
-    // create the initial polygons
-    main_polygon = new Polygon(width / 2, height / 2, global_size, "c_diatonic")
-    neighbors = main_polygon.getNeighbors();
+    nav = new Navigator();
 }
 
 function draw() {
     background(255);
-    var allPolygons = [main_polygon].concat(preview_polygons, old_neighbors)
-    allPolygons.push(...neighbors)
-    allPolygons.push(old_main_polygon)
 
-    //draw all the polygons
-    for (var p of allPolygons) {
-        if (p) p.draw();
-    }
+    nav.draw();
 }
 
 function mousePressed() {
-    // check for clicks on all polygons
-    for (var p of neighbors) {
-        if (p && p.click() && !p.animation.active) {
-            preview_polygons = p.getNeighbors()
-            last_clicked_polygon = p;
-
-            // duplicates between neighbors
-            var all_current = neighbors.concat([main_polygon])
-            actually_new_polygons = new Set();
-            for (var n = 0; n < all_current.length; n++) {
-                for (var pre = 0; pre < preview_polygons.length; pre++) {
-                    if (all_current[n].isMatching(preview_polygons[pre])) {
-                        preview_polygons[pre] = all_current[n];
-                    }
-                }
-            }
-
-            // find polygons which are actually new
-            for (var pre = 0; pre < preview_polygons.length; pre++) {
-                if (!all_current.includes(preview_polygons[pre])) {
-                    actually_new_polygons.add(preview_polygons[pre])
-                }
-            }
-
-            // convert to array
-            actually_new_polygons = Array.from(actually_new_polygons)
-
-            // take care of the fanning out (not all the way around)
-            var total_poly = neighbors.length;
-            var ind = main_polygon.getNeighbors().findIndex(x => x.isMatching(p))
-                //var positions = p.getNeighborPositions(p.x, p.y, p.radius, undefined, undefined, PI / 2 + (2 * PI * (ind - 0.5)) / total_poly, PI / 2 + (2 * PI * (ind + 0.5)) / total_poly, actually_new_polygons.length)
-            var positions = p.getNeighborPositions(p.x, p.y, p.radius, undefined, undefined, PI / 2, PI / 2 + (2 * PI), actually_new_polygons.length)
-
-            /*
-            //position them
-            for (var a_n = 0; a_n < actually_new_polygons.length; a_n++) {
-                var pol = preview_polygons.find(x => actually_new_polygons[a_n].isMatching(x))
-
-                pol.set(["x", positions[a_n].x], ["y", positions[a_n].y], ["size", positions[a_n].size])
-            }*/
-
-            for (var prev of actually_new_polygons) {
-                var oldx = prev.x;
-                var oldy = prev.y;
-
-                prev.set(["x", p.x], ["y", p.y])
-                prev.move(oldx, oldy, prev.size)
-            }
-
-            preview_polygons_ready = true;
-            return
-        }
-    }
+    nav.mousePressed();
 }
 
 function mouseReleased() {
-    // check for clicks on all polygons
-    for (var p of neighbors) {
-        if (p && p.click() && !p.animation.active && preview_polygons_ready) {
-            changeMainScale(p)
-            return
-        }
-    }
-
-    for (var prev of actually_new_polygons) {
-        if (!prev.animation.active) prev.move(last_clicked_polygon.x, last_clicked_polygon.y, 1, 0)
-    }
-
-    //preview_polygons = []
-    preview_polygons_ready = false;
-}
-
-function changeMainScale(new_main, all_duration = 1) {
-    // push the current polygons into old polygons
-    old_neighbors = [...neighbors]
-    old_main_polygon = main_polygon;
-
-    main_polygon = new_main
-    neighbors = preview_polygons;
-
-    //Handle duplicates
-    old_neighbors.splice(old_neighbors.indexOf(main_polygon), 1)
-
-    // duplicate of main polygon
-    var index = neighbors.findIndex((x) => { return old_main_polygon.isMatching(x) });
-    if (index !== -1) {
-        neighbors[index] = old_main_polygon;
-    }
-
-    // Main polygons animation
-    main_polygon.move(width / 2, height / 2, all_duration, global_size);
-    //old_main_polygon.move(width / 2, height / 2, all_duration, 0, 0)
-
-    var positions = main_polygon.getNeighborPositions(width / 2, height / 2, global_size)
-    for (var i = 0; i < neighbors.length; i++) {
-        try {
-            neighbors[i].move(positions[i].x, positions[i].y, all_duration, positions[i].size, 1)
-        } catch (error) {}
-    }
-
-    // Neighboring polygons animation
-    for (var old of old_neighbors) {
-        if (neighbors.findIndex(x => old.isMatching(x)) == -1) {
-            old.move(old_main_polygon.animation.target.x, old_main_polygon.animation.target.y, all_duration, 0, 1)
-        }
-    }
+    nav.mouseReleased();
 }
