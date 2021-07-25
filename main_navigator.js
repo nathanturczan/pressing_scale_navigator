0b2137bce6dc42b5fd7181644e2f2e452905c970
rudytak <ondra4sedlacek@gmail.com>
2021-07-25 23:33:19 +0200

    Added autopilot

--- main_navigator.js
+++ main_navigator.js
@@ -14,6 +14,49 @@ class Navigator {
         // create the initial polygons
         this.main_polygon = new Polygon(width / 2, height / 2, this.poly_size, "c_diatonic")
         this.neighbors = this.main_polygon.getNeighbors();
+
+        this.autopilot_data = {
+            active: false,
+            default_period: 1000,
+            period: undefined,
+            intervalID: undefined
+        }
+
+        this.init_autopilot()
+    }
+
+    init_autopilot() {
+        if (!this.autopilot_data.period) this.autopilot_data.period = this.autopilot_data.default_period
+
+        this.autopilot_data.intervalID = setInterval(() => {
+            if (this.autopilot_data.active) {
+                var p = random(this.neighbors)
+
+                this.changeMainScale(p, min(1, this.autopilot_data.period / 1000))
+                return
+            }
+        }, this.autopilot_data.period)
+    }
+
+    toggle_autopilot(forced_value = undefined) {
+        if (forced_value) {
+            this.autopilot_data.active = forced_value
+        } else {
+            this.autopilot_data.active = !this.autopilot_data.active
+        }
+    }
+
+    reset_autopilot() {
+        this.autopilot_data.active = false;
+        this.set_autopilot_period(undefined)
+    }
+
+    set_autopilot_period(new_period) {
+        this.autopilot_data.period = new_period;
+
+        clearInterval(this.autopilot_data.intervalID)
+
+        this.init_autopilot();
     }
 
     draw() {
@@ -36,53 +79,7 @@ class Navigator {
         // check for clicks on all polygons
         for (var p of this.neighbors) {
             if (p && p.click() && !p.animation.active) {
-                this.preview_polygons = p.getNeighbors()
-                this.last_clicked_polygon = p;
-
-                // duplicates between this.neighbors
-                var all_current = this.neighbors.concat([this.main_polygon])
-                this.actually_new_polygons = new Set();
-                for (var n = 0; n < all_current.length; n++) {
-                    for (var pre = 0; pre < this.preview_polygons.length; pre++) {
-                        if (all_current[n].isMatching(this.preview_polygons[pre])) {
-                            this.preview_polygons[pre] = all_current[n];
-                        }
-                    }
-                }
-
-                // find polygons which are actually new
-                for (var pre = 0; pre < this.preview_polygons.length; pre++) {
-                    if (!all_current.includes(this.preview_polygons[pre])) {
-                        this.actually_new_polygons.add(this.preview_polygons[pre])
-                    }
-                }
-
-                // convert to array
-                this.actually_new_polygons = Array.from(this.actually_new_polygons)
-
-                // take care of the fanning out (not all the way around)
-                var total_poly = this.neighbors.length;
-                var ind = this.main_polygon.getNeighbors().findIndex(x => x.isMatching(p))
-                    //var positions = p.getNeighborPositions(p.x, p.y, p.radius, undefined, undefined, PI / 2 + (2 * PI * (ind - 0.5)) / total_poly, PI / 2 + (2 * PI * (ind + 0.5)) / total_poly, this.actually_new_polygons.length)
-                var positions = p.getNeighborPositions(p.x, p.y, p.radius, undefined, undefined, PI / 2, PI / 2 + (2 * PI), this.actually_new_polygons.length)
-
-                /*
-                //position them
-                for (var a_n = 0; a_n < this.actually_new_polygons.length; a_n++) {
-                    var pol = this.preview_polygons.find(x => this.actually_new_polygons[a_n].isMatching(x))
-    
-                    pol.set(["x", positions[a_n].x], ["y", positions[a_n].y], ["size", positions[a_n].size])
-                }*/
-
-                for (var prev of this.actually_new_polygons) {
-                    var oldx = prev.x;
-                    var oldy = prev.y;
-
-                    prev.set(["x", p.x], ["y", p.y])
-                    prev.move(oldx, oldy, prev.size)
-                }
-
-                this.preview_polygons_ready = true;
+                this.prepareChangeMainScale(p);
                 return
             }
         }
@@ -92,22 +89,64 @@ class Navigator {
         // check for clicks on all polygons
         for (var p of this.neighbors) {
             if (p && p.click() && !p.animation.active && this.preview_polygons_ready) {
-                this.changeMainScale(p)
+                this.finishChangeMainScale(p)
                 return
             }
         }
+    }
 
-        if (this.actually_new_polygons) {
-            for (var prev of this.actually_new_polygons) {
-                if (!prev.animation.active) prev.move(this.last_clicked_polygon.x, this.last_clicked_polygon.y, 1, 0)
+    prepareChangeMainScale(p) {
+        this.preview_polygons = p.getNeighbors()
+        this.last_clicked_polygon = p;
+
+        // duplicates between this.neighbors
+        var all_current = this.neighbors.concat([this.main_polygon])
+        this.actually_new_polygons = new Set();
+        for (var n = 0; n < all_current.length; n++) {
+            for (var pre = 0; pre < this.preview_polygons.length; pre++) {
+                if (all_current[n].isMatching(this.preview_polygons[pre])) {
+                    this.preview_polygons[pre] = all_current[n];
+                }
             }
         }
 
-        //this.preview_polygons = []
-        this.preview_polygons_ready = false;
+        // find polygons which are actually new
+        for (var pre = 0; pre < this.preview_polygons.length; pre++) {
+            if (!all_current.includes(this.preview_polygons[pre])) {
+                this.actually_new_polygons.add(this.preview_polygons[pre])
+            }
+        }
+
+        // convert to array
+        this.actually_new_polygons = Array.from(this.actually_new_polygons)
+
+        // take care of the fanning out (not all the way around)
+        var total_poly = this.neighbors.length;
+        var ind = this.main_polygon.getNeighbors().findIndex(x => x.isMatching(p))
+            //var positions = p.getNeighborPositions(p.x, p.y, p.radius, undefined, undefined, PI / 2 + (2 * PI * (ind - 0.5)) / total_poly, PI / 2 + (2 * PI * (ind + 0.5)) / total_poly, this.actually_new_polygons.length)
+        var positions = p.getNeighborPositions(p.x, p.y, p.radius, undefined, undefined, PI / 2, PI / 2 + (2 * PI), this.actually_new_polygons.length)
+
+        /*
+        //position them
+        for (var a_n = 0; a_n < this.actually_new_polygons.length; a_n++) {
+            var pol = this.preview_polygons.find(x => this.actually_new_polygons[a_n].isMatching(x))
+
+            pol.set(["x", positions[a_n].x], ["y", positions[a_n].y], ["size", positions[a_n].size])
+        }*/
+
+        for (var prev of this.actually_new_polygons) {
+            var oldx = prev.x;
+            var oldy = prev.y;
+
+            prev.set(["x", p.x], ["y", p.y])
+            prev.move(oldx, oldy, prev.size)
+        }
+
+        this.preview_polygons_ready = true;
+        return
     }
 
-    changeMainScale(new_main, all_duration = 1) {
+    finishChangeMainScale(new_main, all_duration = 1) {
         if (new_main == this.main_polygon) return
 
         // push the current polygons into old polygons
@@ -143,5 +182,19 @@ class Navigator {
                 old.move(this.old_main_polygon.animation.target.x, this.old_main_polygon.animation.target.y, all_duration, 0, 1)
             }
         }
+
+        if (this.actually_new_polygons) {
+            for (var prev of this.actually_new_polygons) {
+                if (!prev.animation.active) prev.move(this.last_clicked_polygon.x, this.last_clicked_polygon.y, 1, 0)
+            }
+        }
+
+        //this.preview_polygons = []
+        this.preview_polygons_ready = false;
+    }
+
+    changeMainScale(new_main, all_duration = 1) {
+        this.prepareChangeMainScale(new_main)
+        this.finishChangeMainScale(new_main, all_duration)
     }
 }
